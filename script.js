a0ffce55d64c0ccd9ee14ea76db84f3e4a2c3cdf
brahimelhabchi09-lgtm <brahimelhabchi09@gmail.com>
2025-11-14 16:49:51 +0100

    les consts et le fetch de Games

---
+++ script.js
@@ -0,0 +1,28 @@
+    const apiUrl = "https://debuggers-games-api.duckdns.org/api/games";
+    const gamesContainer = document.getElementById("gamesContainer");
+    const pagination = document.getElementById("pagination");
+    const searchInput = document.getElementById("searchInput");
+    const genreFilter = document.getElementById("genreFilter");
+    const modal = document.getElementById("modal");
+    const closeModal = document.getElementById("closeModal");
+    const modalImg = document.getElementById("modalImg");
+    const modalTitle = document.getElementById("modalTitle");
+    const modalGenre = document.getElementById("modalGenre");
+    const modalDesc = document.getElementById("modalDesc");
+
+    let games = [];
+    let filteredGames = [];
+    let currentPage = 1;
+    const itemsPerPage = 20;
+
+    async function fetchGames() {
+      try {
+        const res = await fetch('https://debuggers-games-api.duckdns.org/api/games?limit=4002');
+        games = await res.json();
+        filteredGames = games.results;
+        displayGames();
+        setupPagination();
+      } catch (err) {
+        gamesContainer.innerHTML = `<p class='text-red-400'>⚠️ Error loading games.</p>`;
+      }
+    }
